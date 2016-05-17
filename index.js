b06e46e1c3e10fe3c15bf564e3bf0f068547743b
Doug Bromley <doug@tintophat.com>
2016-05-17 04:45:01 +0100

    Updated source to bring into line with Hexo style guides

--- index.js
+++ index.js
@@ -4,45 +4,45 @@
  * @author Doug Bromley
  * @license MIT
  */
-hexo.extend.tag.register('deezer', function(args){
+hexo.extend.tag.register('deezer', function(args) {
 
-    var url = 'http://www.deezer.com/plugins/player';
+  var url = 'http://www.deezer.com/plugins/player';
 
-    var trackId = args[0],
-        format = args[1] || 'classic',
-        size = args[2] || 'medium',
-        autoplay = args[3] || 'false',
-        playlist = args[4] || 'false';
+  var trackId = args[0];
+  var format = args[1] || 'classic';
+  var size = args[2] || 'medium';
+  var autoplay = args[3] || 'false';
+  var playlist = args[4] || 'false';
 
-    var width = '700';
-    var height = '120';
+  var width = '700';
+  var height = '120';
 
-    if (format === 'square') {
-        width = '300';
-        height = '300';
-    }
+  if (format === 'square') {
+    width = '300';
+    height = '300';
+  }
 
-    var config = hexo.config.deezer || {};
-    config.theme = config.theme || 'dark';
-    config.colour = config.colour || '1990DB';
+  var config = hexo.config.deezer || {};
+  config.theme = config.theme || 'dark';
+  config.colour = config.colour || '1990DB';
 
-    var iframeTag = '<iframe src="' + url +
-        '?format=' + format +
-        '&autoplay=' + autoplay +
-        '&playlist=' + playlist +
-        '&width=' + width +
-        '&height=' + height +
-        '&color=' + config.colour +
-        '&layout=' + config.theme +
-        '&size=' + size +
-        '&type=tracks' +
-        '&id=' + trackId +
-        '&title=&app_id=161645"' +
-        ' scrolling="no"' +
-        ' frameborder="0"' +
-        ' allowTransparency="true"' +
-        ' width="' + width + '" height="' + height + '">' +
-        '</iframe>';
+  var iframeTag = '<iframe src="' + url +
+      '?format=' + format +
+      '&autoplay=' + autoplay +
+      '&playlist=' + playlist +
+      '&width=' + width +
+      '&height=' + height +
+      '&color=' + config.colour +
+      '&layout=' + config.theme +
+      '&size=' + size +
+      '&type=tracks' +
+      '&id=' + trackId +
+      '&title=&app_id=161645"' +
+      ' scrolling="no"' +
+      ' frameborder="0"' +
+      ' allowTransparency="true"' +
+      ' width="' + width + '" height="' + height + '">' +
+      '</iframe>';
 
-    return iframeTag;
+  return iframeTag;
 });
